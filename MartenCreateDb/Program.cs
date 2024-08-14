4cf0b48bc9adc012f71ae7cacc778a84f0f33e7a
Valeriu Caraulean <Valeriu_Caraulean@cargill.com>
2024-08-14 10:12:08 +0200

    Small readme

--- MartenCreateDb/Program.cs
+++ MartenCreateDb/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Marten;
 
-Console.WriteLine("Hello, World!");
+const string connectionString = "Host=localhost;Database=db_1;Username=pgu;Password=pgp;";
+
+await using var store = DocumentStore.For(options =>
+{
+    options.Connection(connectionString);
+    options.CreateDatabasesForTenants(t =>
+    {
+        t.ForTenant()
+            .DropExisting(true)
+            .CheckAgainstPgDatabase()
+            .WithEncoding("UTF-8")
+            .ConnectionLimit(-1);
+    });
+});
+
+await using var session = store.LightweightSession();
+await session.QueryAsync<int>("select 1");
+
+Console.WriteLine("done");
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Demo project using Marted to create a new database from a console application
+
+`DocumentStore.For<>` is not creating the database from connection string. 
+
+`select 1` throw following error
+
+```
+Marten.Exceptions.MartenCommandException: Marten Command Failure:
+3D000: database "db_1" does not exist
+---> Npgsql.PostgresException (0x80004005): 3D000: database "db_1" does not exist```
