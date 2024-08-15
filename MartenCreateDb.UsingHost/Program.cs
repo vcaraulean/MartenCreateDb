083586b7bf66678d3a63e47db6f37021bea8721e
Valeriu Caraulean <Valeriu_Caraulean@cargill.com>
2024-08-15 11:36:55 +0200

    Another example using IHost

---
+++ MartenCreateDb.UsingHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using Marten;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+const string connectionString = "Host=localhost;Database=db_1;Username=pgu;Password=pgp;";
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+    options.CreateDatabasesForTenants(x =>
+    {
+        x
+            .ForTenant()
+            .CheckAgainstPgDatabase()
+            .WithOwner("postgres")
+            .WithEncoding("UTF-8")
+            .ConnectionLimit(-1);
+    });
+});
+
+using var host = builder.Build();
+
+await host.StartAsync();
+
+using var store = host.Services.GetRequiredService<IDocumentStore>();
+await using var session =  store.LightweightSession();
+await session.QueryAsync<int>("select 1");
+
+Console.WriteLine("done");
